ee57b7132b8a4e4449d0748a7310d8fd03401e5a
dev-variety <developer.variety@gmail.com>
2019-06-14 02:31:11 -0400

    Update Handshake.cs

--- SecureApp/SecureApp/Core/Command/Handshake.cs
+++ SecureApp/SecureApp/Core/Command/Handshake.cs
@@ -1,13 +1,32 @@
 using System;
 using SecureApp.Model.Interface;
+using SecureApp.Model.Networking;
+using SecureAppUtil.Extensions.Networking;
 
 namespace SecureApp.Core.Command
 {
     public class Handshake : ICommand
     {
-        public void Work()
+        public void Work(object[] payload, ClientSession clientSession, Socket.Server.SocketClient socketClient)
         {
-            throw new NotImplementedException();
+            string decryptionCode = (string)payload[2];
+
+            if (!decryptionCode.Equals("app"))
+                // do something, maybe a blacklist for x minutes?
+                Console.WriteLine("waiting..");
+
+            clientSession.Handshake = true;
+            
+            string server = "\x9E\x8D\x9A\x81\x8E\x91";
+ 
+            for (int ONXjG = 0, KnCmq = 0; ONXjG < 6; ONXjG++)
+            {
+                KnCmq = server[ONXjG];
+                KnCmq ^= 0xE8;
+                server = server.Substring(0, ONXjG) + (char)(KnCmq & 0xFF) + server.Substring(ONXjG + 1);
+            }
+
+            socketClient.Send(Guid.Empty, "Handshake", server);
         }
     }
 }
