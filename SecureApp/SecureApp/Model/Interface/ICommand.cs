103cb4edde50487b0de904948f1d46c1ff7f59f2
dev-variety <developer.variety@gmail.com>
2019-06-14 02:30:54 -0400

    Update ICommand.cs

--- SecureApp/SecureApp/Model/Interface/ICommand.cs
+++ SecureApp/SecureApp/Model/Interface/ICommand.cs
@@ -1,7 +1,10 @@
+using SecureApp.Model.Networking;
+using SecureAppUtil.Extensions.Networking;
+
 namespace SecureApp.Model.Interface
 {
     public interface ICommand
     {
-        void Work();
+        void Work(object[] payload, ClientSession clientSession, Socket.Server.SocketClient socketClient);
     }
 }
