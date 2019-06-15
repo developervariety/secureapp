c556b7ca57a74fb26c997f3dfaeeec39b5d831f6
dev-variety <developer.variety@gmail.com>
2019-06-15 03:01:53 -0400

    Revamping Handshake

--- SecureApp/SecureApp/Core/Command/Handshake.cs
+++ SecureApp/SecureApp/Core/Command/Handshake.cs
@@ -9,23 +9,15 @@ namespace SecureApp.Core.Command
     {
         public void Work(object[] payload, ClientSession clientSession, Socket.Server.SocketClient socketClient)
         {
-            string decryptionCode = (string)payload[2];
+            string polyString = Global.Polymorphic.Decrypt((string)payload[2], "dasdasd");
 
-            if (!decryptionCode.Equals("app"))
+            if (!polyString.Equals("app"))
                 // do something, maybe a blacklist for x minutes?
                 Console.WriteLine("waiting..");
 
             clientSession.Handshake = true;
             
-            string server = "\u1803\u6003\u4803\u2803\u7003\uA003";
- 
-            for (int LwloM = 0, LuZMc = 0; LwloM < 6; LwloM++)
-            {
-                LuZMc = server[LwloM];
-                LuZMc = ((LuZMc << 5) | ( (LuZMc & 0xFFFF) >> 11)) & 0xFFFF;
-                server = server.Substring(0, LwloM) + (char)(LuZMc & 0xFFFF) + server.Substring(LwloM + 1);
-            }
-            socketClient.Send(Guid.Empty, "Handshake", server);
+            socketClient.Send(Guid.Empty, "Handshake");
         }
     }
 }
