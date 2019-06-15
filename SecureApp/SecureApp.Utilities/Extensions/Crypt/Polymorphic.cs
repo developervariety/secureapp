03e2914d04df83a48649d99a8a2f0278aa479c70
dev-variety <developer.variety@gmail.com>
2019-06-15 03:00:28 -0400

    Add Global.cs and Polymorphic encryption

---
+++ SecureApp/SecureApp.Utilities/Extensions/Crypt/Polymorphic.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace SecureAppUtil.Extensions.Crypt
+{    
+    public class Polymorphic
+    {
+        public string Encrypt(string input, string key)
+        {
+            System.Random r = new System.Random();
+            int kc = 0;
+            char[] text = input.ToCharArray();
+            char[] keyarr = key.ToCharArray();
+            char[] finVal = new char[input.Length + 1];
+            int rnd = r.Next(100, 220);
+            for (int index = 0; index < input.Length; index++)
+            {
+                if (kc >= keyarr.Length)
+                    kc = 0;
+
+                int ptVal = text[index];
+                int kVal = keyarr[kc];
+                int ciVal = ptVal + kVal + rnd;
+                finVal[index] = Convert.ToChar(ciVal);
+                kc++;
+            }
+            finVal[input.Length] = (char)rnd;
+            string retStr = new string(finVal);
+            return retStr;
+
+        }
+
+        public string Decrypt(string input, string key)
+        {
+            char[] text = input.ToCharArray();
+            char[] keyarr = key.ToCharArray();
+            char[] finVal = new char[input.Length - 1];
+            int rndKVal = text[input.Length - 1];
+            text[input.Length - 1] = '\0';
+            int kc = 0;
+            for (int index = 0; index < input.Length; index++)
+            {
+                if (index >= input.Length - 1)
+                    continue;
+                if (kc >= keyarr.Length)
+                    kc = 0;
+                int ciVal = text[index];
+                int kVal = keyarr[kc];
+                int ptVal = ciVal - rndKVal - kVal;
+                finVal[index] = Convert.ToChar(ptVal);
+                kc++;
+            }
+            string retStr = new string(finVal);
+            return retStr;
+        }
+    }
+}
---
+++ SecureApp/SecureApp/Core/Global.cs
@@ -0,0 +1,9 @@
+using SecureAppUtil.Extensions.Crypt;
+
+namespace SecureApp.Core
+{
+    public class Global
+    {
+        public static readonly Polymorphic Polymorphic = new Polymorphic();
+    }
+}
