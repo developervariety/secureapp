11fe35527bf9ef0452de7306b731db239f24f18d
dev-variety <developer.variety@gmail.com>
2019-06-18 00:42:38 -0400

    Add HMACSHA256

--- SecureApp/SecureApp.Utilities/Crypt/Hash.cs
+++ SecureApp/SecureApp.Utilities/Crypt/Hash.cs
@@ -1,16 +1,28 @@
 using System.Security.Cryptography;
 using System.Text;
+using SecureAppUtil.Extensions;
 
 namespace SecureAppUtil.Crypt
 {
-    public static class Hash
+    public class Hash
     {
         public static byte[] Sha256(string value)
         {
             byte[] strBytes = Encoding.UTF8.GetBytes(value);
-            using (SHA256CryptoServiceProvider cryptoServiceProvider = new SHA256CryptoServiceProvider())
+            using (SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider())
             {
-                return cryptoServiceProvider.ComputeHash(strBytes);
+                return sha256.ComputeHash(strBytes);
+            }
+        }
+
+        public static string HmacSha256(string value, string key)
+        {
+            byte[] strBytes = Encoding.UTF8.GetBytes(value);
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            using (HMACSHA256 hmac = new HMACSHA256(keyBytes))
+            {
+                byte[] bytes = hmac.ComputeHash(strBytes);
+                return Base32.Encode(bytes);
             }
         }
     }
