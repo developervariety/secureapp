0cfc349f16b6ac9f3bea45dcae20f1d11eaa6d2b
dev-variety <developer.variety@gmail.com>
2019-06-18 19:44:58 -0400

    Update Settings.cs

--- SecureApp/SecureApp/Settings.cs
+++ SecureApp/SecureApp/Settings.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using SecureAppUtil.Model.Interface;
+
 namespace SecureApp
 {
     public class Settings
@@ -7,5 +10,7 @@ namespace SecureApp
             public static string PublicKey { get; set; }
             public static string PrivateKey { get; set; }
         }
+        
+        public static List<IPlugin> Plugins { get; set; }
     }
 }
