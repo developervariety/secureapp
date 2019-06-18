273a96bae8ff7206fe3d2a71371c8becf858ca49
dev-variety <developer.variety@gmail.com>
2019-06-18 00:40:55 -0400

    Update LicenseKeyGenerator

--- Tests/LicenseKeyGenerator/Program.cs
+++ Tests/LicenseKeyGenerator/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using SecureAppUtil;
-using SecureAppUtil.Extensions;
+using SecureAppUtil.Core;
 using SecureAppUtil.Model;
 using SecureAppUtil.Model.Enum.License;
 using Random = SecureAppUtil.Extensions.Random;
@@ -10,7 +9,7 @@ namespace LicenseKeyGenerator
     internal static class Program
     {
         private static readonly Generator Generator = new Generator("secureapp123!stongpassword");
-        
+
         public static void Main()
         {
             LicenseKey data = new LicenseKey
@@ -23,13 +22,13 @@ namespace LicenseKeyGenerator
 
             string licenseKey = Generator.Generate(data);
             Console.WriteLine(licenseKey);
-            
+
             LicenseKey license = new LicenseKey();
             string status = Generator.Validate(licenseKey, ref license) ? "valid" : "not valid";
-                        
+
             Console.Write($"This license key is {status}.");
             Console.WriteLine(Environment.NewLine);
-            
+
             Console.WriteLine($"AppId: {data.ApplicationId}");
             Console.WriteLine($"Tier: {data.Tier}");
             Console.WriteLine($"Edition: {data.Edition}");
