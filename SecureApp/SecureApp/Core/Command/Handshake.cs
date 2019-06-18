4cb0af3b6320611f50be682b8899051dce2c0714
dev-variety <developer.variety@gmail.com>
2019-06-18 00:45:05 -0400

    Update commands

--- SecureApp/SecureApp/Core/Command/Handshake.cs
+++ SecureApp/SecureApp/Core/Command/Handshake.cs
@@ -7,16 +7,16 @@ namespace SecureApp.Core.Command
 {
     public class Handshake : ICommand
     {
-        public void Work(object[] payload, ClientSession clientSession, Socket.Server.SocketClient socketClient)
+        public void Work(object[] payload, ClientSession clientSession, SecureSocket.Server.SocketClient socketClient)
         {
-            string polyString = Global.Polymorphic.Decrypt((string)payload[2], "dasdasd");
+            string polyString = Global.Polymorphic.Decrypt((string) payload[2], "dasdasd");
 
             if (!polyString.Equals("app"))
                 // do something, maybe a blacklist for x minutes?
                 Console.WriteLine("waiting..");
 
             clientSession.Handshake = true;
-            
+
             socketClient.Send(Guid.Empty, "Handshake");
         }
     }
--- SecureApp/SecureApp/Core/Command/SharedSecret.cs
+++ SecureApp/SecureApp/Core/Command/SharedSecret.cs
@@ -7,7 +7,7 @@ namespace SecureApp.Core.Command
 {
     public class SharedSecret : ICommand
     {
-        public void Work(object[] payload, ClientSession clientSession, Socket.Server.SocketClient socketClient)
+        public void Work(object[] payload, ClientSession clientSession, SecureSocket.Server.SocketClient socketClient)
         {
             // TODO:: create a method to check if handshake is valid and if not remove the client and add to a blacklist
 
@@ -15,7 +15,7 @@ namespace SecureApp.Core.Command
             clientSession.Encryption.Enabled = true;
 
             socketClient.Encryption = clientSession.Encryption;
-            
+
             socketClient.Send(Guid.Empty, "SharedSecret", "Successfully shared the key.");
         }
     }
