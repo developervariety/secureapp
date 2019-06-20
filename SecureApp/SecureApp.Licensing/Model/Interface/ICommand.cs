749aca382fa6d942821c866ace4f109a6177da77
dev-variety <developer.variety@gmail.com>
2019-06-20 15:20:38 -0400

    Remove files

--- SecureApp/SecureApp.Licensing/Model/Interface/ICommand.cs
+++
@@ -1,9 +0,0 @@
-using SecureAppUtil.Networking;
-
-namespace SecureApp.Licensing.Model.Interface
-{
-    public interface ICommand
-    {
-        void Work(object[] payload, SecureSocket.Client socket);
-    }
-}
--- SecureApp/SecureApp.Utilities/Model/Interface/ISocketEncryption.cs
+++
@@ -1,8 +0,0 @@
-namespace SecureAppUtil.Model.Interface
-{
-    public interface ISocketEncryption
-    {
-        byte[] Encrypt(byte[] input, string key);
-        byte[] Decrypt(byte[] input, string key);
-    }
-}
