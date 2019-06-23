fb2824c367f64ba0545902561b18339687e17141
dev-variety <developer.variety@gmail.com>
2019-06-23 14:19:54 -0400

    Communicate with API

--- php/app/Controller/Authentication.php
+++ php/app/Controller/Authentication.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Requests;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -14,6 +15,21 @@ class Authentication extends Controller
 
     public function register(Request $request, Response $response)
     {
+        return $this->view->render($response, "auth/register.twig");
+    }
+
+    public function postRegister(Request $request, Response $response)
+    {
+        $headers = array("Accept" => "application/json");
+        $options = $request->getParams();
+
+        $request = Requests::post("https://secureapp.developer-variety.com/api/auth/register", $headers, $options);
+        $body = json_decode($request->body, true);
+
+        if ($body["status"] == "error") {
+            $_SESSION["errors"] = $body["data"];
+        }
+
         return $this->view->render($response, "auth/register.twig");
     }
 }
