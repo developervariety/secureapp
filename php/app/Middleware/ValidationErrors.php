3d65c7704e2e6adb4e73b5080523c0361c2f099a
dev-variety <developer.variety@gmail.com>
2019-06-23 14:20:02 -0400

    Fix bug

--- php/app/Middleware/ValidationErrors.php
+++ php/app/Middleware/ValidationErrors.php
@@ -6,7 +6,7 @@ class ValidationErrors extends Middleware
 {
     public function __invoke($request, $response, $next)
     {
-        $this->container->view->getEnvironment()->addGlobal("errors", $_SESSION);
+        $this->container->view->getEnvironment()->addGlobal("errors", $_SESSION["errors"]);
         unset($_SESSION["errors"]);
 
         $response = $next($request, $response);
