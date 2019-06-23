f6ba40bb79cbdfa95447f16c80970cf34846977e
dev-variety <developer.variety@gmail.com>
2019-06-23 15:43:09 -0400

    Update routes.php

--- php/app/Route/website/routes.php
+++ php/app/Route/website/routes.php
@@ -4,3 +4,4 @@ $app->get("/login", "Authentication:login")->setName("login");
 
 $app->get("/register", "Authentication:register")->setName("register");
 $app->post("/register", "Authentication:postRegister");
+
