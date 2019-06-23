e9b948228c1cade5ca3f1234d826971c120a9d41
dev-variety <developer.variety@gmail.com>
2019-06-23 15:43:16 -0400

    Update middleware.php

--- php/bootstrap/middleware.php
+++ php/bootstrap/middleware.php
@@ -1,5 +1,14 @@
 <?php
 
-use App\Middleware\ValidationErrors;
+use RKA\Middleware\IpAddress;
 
-$app->add(new ValidationErrors($container));
+$checkProxyHeaders = isset($container->get("settings")["trustedProxy"])? is_array ($container->get("settings")["trustedProxy"])? true : false : false;
+$checkProxyHeaders = true;
+$app->add(new IpAddress(true, is_array ($container->get("settings")["trustedProxy"])? $container->get("settings")["trustedProxy"] : [], "ipAddress", [
+    "X-Real-IP",
+    "Forwarded",
+    "X-Forwarded-For",
+    "X-Forwarded",
+    "X-Cluster-Client-Ip",
+    "Client-Ip",
+]));
