01c4fe98c54c1f4d4c88ea46df79bc88be0ee7f9
dev-variety <developer.variety@gmail.com>
2019-06-23 04:55:10 -0400

    Update index.php

--- php/public/index.php
+++ php/public/index.php
@@ -2,16 +2,16 @@
 
 $settings = include_once __DIR__ . "/../settings.php";
 
-//if (PHP_SAPI == "cli-server") {
-//    $url  = parse_url($_SERVER["REQUEST_URI"]);
-//    $file = __DIR__ . $url["path"];
-//
-//    if (is_file($file)) {
-//        return false;
-//    }
-//
-//    $settings["displayErrorDetails"] = true;
-//}
+if (PHP_SAPI == "cli-server") {
+    $url  = parse_url($_SERVER["REQUEST_URI"]);
+    $file = __DIR__ . $url["path"];
+
+    if (is_file($file)) {
+        return false;
+    }
+
+    $settings["displayErrorDetails"] = true;
+}
 
 require __DIR__ . "/../bootstrap/app.php";
 
